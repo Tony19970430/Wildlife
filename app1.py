b8681451dd8978bec434fa26de5e940c60c786ec
Tony19970430 <46965728+Tony19970430@users.noreply.github.com>
2019-09-24 10:53:08 +0800

    update

--- app1.py
+++ app1.py
@@ -32,50 +32,331 @@ styles = {
     }
 }
 
-df = pd.read_csv(PATH.joinpath("comptab_2018-01-29 16_00_comma_separated.csv"))
+df = pd.read_csv(PATH.joinpath("result.csv"))
 
 # Create global chart template
 mapbox_access_token = "pk.eyJ1IjoiamFja2x1byIsImEiOiJjajNlcnh3MzEwMHZtMzNueGw3NWw5ZXF5In0.fk8k06T96Ml9CLGgKmk81w"
 
-available_exporter = df['Exporter'].unique()
-exporter_options_list=[{'label': i, 'value': i} for i in available_exporter]+[{'label': 'All', 'value': 'All'}]
-exporter_options_list.pop(35)
-
 available_importer = df['Importer'].unique()
 importer_options_list=[{'label': i, 'value': i} for i in available_importer]+[{'label': 'All', 'value': 'All'}]
+importer_options_list.pop(80)
+
+available_exporter = df['Exporter'].unique()
+exporter_options_list=[{'label': i, 'value': i} for i in available_exporter]+[{'label': 'All', 'value': 'All'}]
+exporter_options_list.pop(45)
+
+available_source = df['Source'].unique()
+source_options_list=[{'label': i, 'value': i} for i in available_source]+[{'label': 'All', 'value': 'All'}]
+source_options_list.pop(0)
+
+available_family = df['Family'].unique()
+family_options_list=[{'label': i, 'value': i} for i in available_family]+[{'label': 'All', 'value': 'All'}]
+family_options_list.pop(35)
+
+available_purpose = df['Purpose'].unique()
+purpose_options_list=[{'label': i, 'value': i} for i in available_purpose]+[{'label': 'All', 'value': 'All'}]
+purpose_options_list.pop(1)
+
+available_app = df['App.'].unique()
+app_options_list=[{'label': i, 'value': i} for i in available_app]+[{'label': 'All', 'value': 'All'}]
+
+available_year = df['Year'].unique()
+year_options_list=[{'label': i, 'value': i} for i in available_year]+[{'label': 'All', 'value': 'All'}]
+
 
 
 app.layout = html.Div([
 
-
     html.Div([
-        html.Div([dcc.Dropdown(id='importer-selector',options=exporter_options_list,value='All')],),
+        html.Div([dcc.Dropdown(id='importer-selector',options=importer_options_list,value='All')],),
         html.Div([html.H4("Importer")], style={"textAlign": "center"})
     ], className ='pretty_container two columns'),
-    
+
     html.Div([
         html.Div([dcc.Dropdown(id='exporter-selector',options=exporter_options_list,value='All')],),
         html.Div([html.H4("Exporter")], style={"textAlign": "center"})
     ], className ='pretty_container two columns'),
 
     html.Div([
+        html.Div([dcc.Dropdown(id='source-selector',options=source_options_list,value='All')],),
+        html.Div([html.H4("Source")], style={"textAlign": "center"})
+    ], className ='pretty_container two columns'),
+
+    html.Div([
+        html.Div([dcc.Dropdown(id='family-selector',options=family_options_list,value='All')],),
+        html.Div([html.H4("Family")], style={"textAlign": "center"})
+    ], className ='pretty_container two columns'),
+
+    html.Div([
+        html.Div([dcc.Dropdown(id='purpose-selector',options=purpose_options_list,value='All')],),
+        html.Div([html.H4("Purpose")], style={"textAlign": "center"})
+    ], className ='pretty_container two columns'),
+
+    html.Div([
+        html.Div([dcc.Dropdown(id='app-selector',options=app_options_list,value='All')],),
+        html.Div([html.H4("Appendix")], style={"textAlign": "center"})
+    ], className ='pretty_container two columns'),
+
+    html.Div([
+        dcc.RangeSlider(
+            id="year-selector",
+            min=1975,
+            max=2018,
+            value=[1990, 2010],
+            ),
+
+        html.Div(id='output-year-selector')], className="pretty_container one-third columns"),
+    
+    html.Div([
+        dcc.Graph(
+            id='pie-chart-importer'
+        )
+    ], className = 'pretty_container four columns'),
+
+    html.Div([
         dcc.Graph(
             id='pie-chart-exporter'
         )
-    ], className = 'pretty_container six columns'),
+    ], className = 'pretty_container four columns'),
 
-], className="pretty_container")
+    html.Div([
+        dcc.Graph(
+            id='pie-chart-source'
+        )
+    ], className = 'pretty_container four columns'),
+
+    html.Div([
+        dcc.Graph(
+            id='pie-chart-family'
+        )
+    ], className = 'pretty_container four columns'),
+
+    html.Div([
+        dcc.Graph(
+            id='pie-chart-purpose'
+        )
+    ], className = 'pretty_container four columns'),
+
+    html.Div([
+        dcc.Graph(
+            id='pie-chart-app'
+        )
+    ], className = 'pretty_container four columns'),
+
+])
+
+@app.callback(
+    dash.dependencies.Output('output-year-selector', 'children'),
+    [dash.dependencies.Input('year-selector', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
 
 
-@app.callback(Output('pie-chart-exporter', 'figure'), [Input('exporter-selector', 'value')])
-def update_graph(selected):
+@app.callback(Output('pie-chart-importer', 'figure'), [Input('importer-selector', 'value'),Input('exporter-selector', 'value'),
+Input('source-selector', 'value'),Input('family-selector', 'value'),Input('purpose-selector', 'value'),Input('app-selector', 'value'),Input('year-selector', 'value')])
 
-    df = pd.read_csv(PATH.joinpath("comptab_2018-01-29 16_00_comma_separated.csv"))
+def update_graph(selected_importer,selected_exporter,selected_source,selected_family,selected_purpose,selected_app,selected_year):
 
-    if selected != 'All':
-        df = df[df["Exporter"] == selected]
+    df = pd.read_csv(PATH.joinpath("result.csv"))
+
+    df = df[(df["Year"] >= selected_year[0])
+        & (df["Year"] <= selected_year[1])
+    ]
+
+    if selected_importer != 'All':
+        df = df[df["Importer"] == selected_importer]
+
+    if selected_exporter != 'All':
+        df = df[df["Exporter"] == selected_exporter]
+
+    if selected_source != 'All':
+        df = df[df["Source"] == selected_source]
+
+    if selected_family != 'All':
+        df = df[df["Family"] == selected_family]
+
+    if selected_purpose != 'All':
+        df = df[df["Purpose"] == selected_purpose]
+
+    if selected_app != 'All':
+        df = df[df["App."] == selected_app]
 
     df_counts = df["Importer"].value_counts().rename_axis('unique_values').reset_index(name='counts')
+
+    return {
+        'data': [go.Pie(
+            labels=df_counts['unique_values'].values.tolist(),
+            values=df_counts["counts"].values.tolist())],
+        "layout": go.Layout(autosize=True)}
+
+@app.callback(Output('pie-chart-exporter', 'figure'), [Input('importer-selector', 'value'),Input('exporter-selector', 'value'),
+Input('source-selector', 'value'),Input('family-selector', 'value'),Input('purpose-selector', 'value'),Input('app-selector', 'value'),Input('year-selector', 'value')])
+
+def update_graph(selected_importer,selected_exporter,selected_source,selected_family,selected_purpose,selected_app,selected_year):
+
+    df = pd.read_csv(PATH.joinpath("result.csv"))
+
+    df = df[(df["Year"] >= selected_year[0])& (df["Year"] <= selected_year[1])]
+
+    if selected_importer != 'All':
+        df = df[df["Importer"] == selected_importer]
+
+    if selected_exporter != 'All':
+        df = df[df["Exporter"] == selected_exporter]
+
+    if selected_source != 'All':
+        df = df[df["Source"] == selected_source]
+
+    if selected_family != 'All':
+        df = df[df["Family"] == selected_family]
+
+    if selected_purpose != 'All':
+        df = df[df["Purpose"] == selected_purpose]
+
+    if selected_app != 'All':
+        df = df[df["App."] == selected_app]
+
+    df_counts = df["Exporter"].value_counts().rename_axis('unique_values').reset_index(name='counts')
+
+    return {
+        'data': [go.Pie(
+            labels=df_counts['unique_values'].values.tolist(),
+            values=df_counts["counts"].values.tolist())],
+        "layout": go.Layout(autosize=True)}
+
+@app.callback(Output('pie-chart-source', 'figure'), [Input('importer-selector', 'value'),Input('exporter-selector', 'value'),
+Input('source-selector', 'value'),Input('family-selector', 'value'),Input('purpose-selector', 'value'),Input('app-selector', 'value'),Input('year-selector', 'value')])
+
+def update_graph(selected_importer,selected_exporter,selected_source,selected_family,selected_purpose,selected_app,selected_year):
+
+    df = pd.read_csv(PATH.joinpath("result.csv"))
+
+    df = df[(df["Year"] >= selected_year[0])& (df["Year"] <= selected_year[1])]
+
+    if selected_importer != 'All':
+        df = df[df["Importer"] == selected_importer]
+
+    if selected_exporter != 'All':
+        df = df[df["Exporter"] == selected_exporter]
+
+    if selected_source != 'All':
+        df = df[df["Source"] == selected_source]
+
+    if selected_family != 'All':
+        df = df[df["Family"] == selected_family]
+
+    if selected_purpose != 'All':
+        df = df[df["Purpose"] == selected_purpose]
+
+    if selected_app != 'All':
+        df = df[df["App."] == selected_app]
+
+    df_counts = df["Source"].value_counts().rename_axis('unique_values').reset_index(name='counts')
+
+    return {
+        'data': [go.Pie(
+            labels=df_counts['unique_values'].values.tolist(),
+            values=df_counts["counts"].values.tolist())],
+        "layout": go.Layout(autosize=True)}
+
+@app.callback(Output('pie-chart-family', 'figure'), [Input('importer-selector', 'value'),Input('exporter-selector', 'value'),
+Input('source-selector', 'value'),Input('family-selector', 'value'),Input('purpose-selector', 'value'),Input('app-selector', 'value'),Input('year-selector', 'value')])
+
+def update_graph(selected_importer,selected_exporter,selected_source,selected_family,selected_purpose,selected_app,selected_year):
+
+    df = pd.read_csv(PATH.joinpath("result.csv"))
+
+    df = df[(df["Year"] >= selected_year[0]) & (df["Year"] <= selected_year[1])]
+
+    if selected_importer != 'All':
+        df = df[df["Importer"] == selected_importer]
+
+    if selected_exporter != 'All':
+        df = df[df["Exporter"] == selected_exporter]
+
+    if selected_source != 'All':
+        df = df[df["Source"] == selected_source]
+
+    if selected_family != 'All':
+        df = df[df["Family"] == selected_family]
+
+    if selected_purpose != 'All':
+        df = df[df["Purpose"] == selected_purpose]
+
+    if selected_app != 'All':
+        df = df[df["App."] == selected_app]
+
+    df_counts = df["Family"].value_counts().rename_axis('unique_values').reset_index(name='counts')
+
+    return {
+        'data': [go.Pie(
+            labels=df_counts['unique_values'].values.tolist(),
+            values=df_counts["counts"].values.tolist())],
+        "layout": go.Layout(showlegend=False,autosize=True)}
+
+@app.callback(Output('pie-chart-purpose', 'figure'), [Input('importer-selector', 'value'),Input('exporter-selector', 'value'),
+Input('source-selector', 'value'),Input('family-selector', 'value'),Input('purpose-selector', 'value'),Input('app-selector', 'value'),Input('year-selector', 'value')])
+
+def update_graph(selected_importer,selected_exporter,selected_source,selected_family,selected_purpose,selected_app,selected_year):
+
+    df = pd.read_csv(PATH.joinpath("result.csv"))
+
+    df = df[(df["Year"] >= selected_year[0]) & (df["Year"] <= selected_year[1])]
+
+    if selected_importer != 'All':
+        df = df[df["Importer"] == selected_importer]
+
+    if selected_exporter != 'All':
+        df = df[df["Exporter"] == selected_exporter]
+
+    if selected_source != 'All':
+        df = df[df["Source"] == selected_source]
+
+    if selected_family != 'All':
+        df = df[df["Family"] == selected_family]
+
+    if selected_purpose != 'All':
+        df = df[df["Purpose"] == selected_purpose]
+
+    if selected_app != 'All':
+        df = df[df["App."] == selected_app]
+
+    df_counts = df["Purpose"].value_counts().rename_axis('unique_values').reset_index(name='counts')
+
+    return {
+        'data': [go.Pie(
+            labels=df_counts['unique_values'].values.tolist(),
+            values=df_counts["counts"].values.tolist())],
+        "layout": go.Layout(autosize=True)}
+
+@app.callback(Output('pie-chart-app', 'figure'), [Input('importer-selector', 'value'),Input('exporter-selector', 'value'),
+Input('source-selector', 'value'),Input('family-selector', 'value'),Input('purpose-selector', 'value'),Input('app-selector', 'value'),Input('year-selector', 'value')])
+
+def update_graph(selected_importer,selected_exporter,selected_source,selected_family,selected_purpose,selected_app,selected_year):
+
+    df = pd.read_csv(PATH.joinpath("result.csv"))
+
+    df = df[(df["Year"] >= selected_year[0]) & (df["Year"] <= selected_year[1])]
+
+    if selected_importer != 'All':
+        df = df[df["Importer"] == selected_importer]
+
+    if selected_exporter != 'All':
+        df = df[df["Exporter"] == selected_exporter]
+
+    if selected_source != 'All':
+        df = df[df["Source"] == selected_source]
+
+    if selected_family != 'All':
+        df = df[df["Family"] == selected_family]
+
+    if selected_purpose != 'All':
+        df = df[df["Purpose"] == selected_purpose]
+
+    if selected_app != 'All':
+        df = df[df["App."] == selected_app]
+
+    df_counts = df["App."].value_counts().rename_axis('unique_values').reset_index(name='counts')
 
     return {
         'data': [go.Pie(
